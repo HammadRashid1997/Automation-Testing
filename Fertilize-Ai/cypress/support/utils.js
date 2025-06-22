7fb48e15153631f40e6d866d14f30b76e7134d3c
Muhammad Hammad Rashid <160396261+HammadRashid1997@users.noreply.github.com>
2025-06-22 11:25:50 +0500

    Update utils.js

--- Fertilize-Ai/cypress/support/utils.js
+++ Fertilize-Ai/cypress/support/utils.js
@@ -1,3 +1,11 @@
 // Helper function file
 
 export const normalize = (str) => str.replace(/\s+/g, ' ').trim();
+
+export const getParaGlobalIndex = (sections, currentSectionIndex, currentParaIndex) => {
+    let count = 0;
+    for (let i = 0; i < currentSectionIndex; i++) {
+        count += sections[i].paragraphs.length;
+    }
+    return count + currentParaIndex;
+};
